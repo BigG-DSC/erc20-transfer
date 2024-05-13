282cd22e67e044aca98d7ffa95f0ee10f8ced889
Gioele Bigini <devbigdsc@gmail.com>
2024-05-13 10:37:45 +0200

    setting up IERC20 interface

---
+++ contracts/interfaces/IERC20.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+interface IERC20 {
+    // name
+    function name() external view returns(string);
+
+    // symbol
+    function symbol() external view returns(string);
+    
+    // decimals
+    function decimals() external view returns(unit256);
+
+    // allowance
+    function allowance(address owner, address spender) external view returns(unit256);
+    
+    // balanceof
+    function balanceof(address account) external view returns(unit256);
+
+    // balanceof
+    function totalSupply() external view returns(unit256);
+    
+    // transfer
+    function transfer(address to, unit256 amount) external returns(bool);
+    
+    // transferFrom
+    function transferFrom(address from, address to, unit256) external returns(bool);
+    
+    // approve
+    function approve(address spender, unit256 amount) external returns(string);
+}
