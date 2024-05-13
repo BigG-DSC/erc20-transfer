ad5f323bfb000f89d00e75942609d9a931478fd9
Gioele Bigini <devbigdsc@gmail.com>
2024-05-13 12:07:49 +0200

    adding deployment script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
+require('dotenv/config');
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    goerli: {
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+      url: '<https://rpc.ankr.com/eth_sepolia>',
+    },
+  },
 };
---
+++ test/ERC20.test.js
@@ -0,0 +1,66 @@
+const { expect } = require("chai");
+
+describe("ERC20 Token Contract", function () {
+  let Token;
+  let hardhatToken;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
+
+  beforeEach(async function () {
+    // Get the ContractFactory and Signers here.
+    Token = await ethers.getContractFactory("ERC20");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    // Deploy a new contract before each test.
+    hardhatToken = await Token.deploy("TestToken", "TTK", 18);
+  });
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      expect(await hardhatToken.balanceOf(owner.address)).to.equal(await hardhatToken.totalSupply());
+    });
+
+    it("Total supply test", async function () {
+      const totalSupply = await hardhatToken.totalSupply();
+      expect(await hardhatToken.balanceOf(owner.address)).to.equal(totalSupply);
+    });
+  });
+
+  describe("Transactions", function () {
+    it("Should transfer tokens between accounts", async function () {
+      // Transfer 50 tokens from owner to addr1
+      await hardhatToken.transfer(addr1.address, 50);
+      expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+      expect(await hardhatToken.balanceOf(owner.address)).to.equal((await hardhatToken.totalSupply()).sub(50));
+    });
+
+    it("Should fail if sender doesn't have enough tokens", async function () {
+      const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
+      // Try to send 1 token from addr1 (0 tokens) to owner (10000 tokens).
+      // `require` will throw if the condition is not met and the transaction will be reverted.
+      await expect(hardhatToken.connect(addr1).transfer(owner.address, 1)).to.be.revertedWith("ERC20: transfer amount exceeds balance");
+    });
+
+    it("Should update balances after transfers", async function () {
+      const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
+
+      // Transfer 100 tokens from owner to addr1.
+      await hardhatToken.transfer(addr1.address, 100);
+
+      // Transfer another 50 tokens from owner to addr2.
+      await hardhatToken.transfer(addr2.address, 50);
+
+      // Check balances.
+      const finalOwnerBalance = await hardhatToken.balanceOf(owner.address);
+      expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(150));
+
+      const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(100);
+
+      const addr2Balance = await hardhatToken.balanceOf(addr2.address);
+      expect(addr2Balance).to.equal(50);
+    });
+  });
+});
